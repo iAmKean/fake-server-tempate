9a012b0d8ddb4d4be49c0cf5b77065f9b7d1dff6
Keanu Dela Cruz <34960738+iAmKean@users.noreply.github.com>
2024-04-12 11:51:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello, World!' });
 });
 
-const port = 7002; // port should be inputted correctly on the app
+const port = 7001; // port should be inputted correctly on the app
 const url = `http://127.0.0.1:${port}/`; // url should be inputted correctly on the app
-const field_url = `http://127.0.0.1:7002/`; // to be automatically inputted when file is selected
+const field_url = `http://127.0.0.1:7001/`; // to be automatically inputted when file is selected
 app.listen(port, () => {
 	console.log(`Server running at ${url}`);
 });
